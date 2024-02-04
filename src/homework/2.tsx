66894398626fe18a79a4f7c61e8589635b2f0a2c
zorianach <zoryanamirchuk@gmail.com>
2024-02-04 16:06:41 +0200

    all tasks

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,5 +1,19 @@
 import React, {useReducer} from "react";
 
+
+type RequestStep = "idle" | "start" | "pending" | "finished";
+
+type Action =
+  | { type: "START_REQUEST" }
+  | { type: "PENDING_REQUEST" }
+  | { type: "FINISH_REQUEST" }
+  | { type: "RESET_REQUEST" };
+
+type State = {
+  isRequestInProgress: boolean;
+  requestStep: RequestStep;
+};
+
 const initialState: State = {
   isRequestInProgress: false,
   requestStep: 'idle',
